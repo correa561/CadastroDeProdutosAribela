a0399ba49056857ae506e0f486d0d2b627d023ae
Corrêa <jovicorreabz@gmail.com>
2025-06-30 17:35:03 -0400

    first commit

---
+++ src/main/java/dev/com/correa/CadastroDeProdutosAribela/CadastroDeProdutosAribelaApplication.java
@@ -0,0 +1,13 @@
+package dev.com.correa.CadastroDeProdutosAribela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeProdutosAribelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeProdutosAribelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/com/correa/CadastroDeProdutosAribela/CadastroDeProdutosAribelaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.com.correa.CadastroDeProdutosAribela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeProdutosAribelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
