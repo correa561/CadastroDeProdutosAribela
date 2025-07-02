033ecb0283ceeca99adf6008619f6123e7f8a861
Corrêa <jovicorreabz@gmail.com>
2025-07-02 11:14:24 -0400

    feat : implementa entidade ProdutoModel, UsuarioModel com Lombok e JPA  -Adiciona anota

--- src/main/java/dev/com/correa/CadastroDeProdutosAribela/UsuarioController.java
+++ src/main/java/dev/com/correa/CadastroDeProdutosAribela/UsuarioController.java
@@ -1,4 +1,4 @@
-package dev.com.correa.CadastroDeProdutosAribela.Usuarios;
+package dev.com.correa.CadastroDeProdutosAribela;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
